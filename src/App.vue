554069741da21bc0cea61a135a7ca0e6f852f23e
pabloporzio <pabloporzio97@gmail.com>
2021-10-13 15:39:52 -0300

    Chapter 2 - Lesson 11 - Navigation Component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
-<script setup>
-</script>
-
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
+  <TheNavigation />
   <div class="container">
     <router-view />
   </div>
 </template>
+
+
+<script>
+import TheNavigation from '@/components/TheNavigation.vue';
+
+export default {
+    components: {
+        TheNavigation,
+    }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,5 +23,5 @@
                 destinations: sourceData.destinations,
             }
         }
-    }
+    };
 </script>
