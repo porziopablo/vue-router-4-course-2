02c1288d5e43fb3c4e3808640dc69797ac66c6c7
pabloporzio <pabloporzio97@gmail.com>
2021-10-13 14:35:51 -0300

    Chapter 2 - Lesson 7 - Adding Routes and Content

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,14 @@
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <br>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-  <router-view />
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </div>
+  <div class="container">
+    <router-view />
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
