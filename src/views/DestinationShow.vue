b7ddcdeff9bc4c9e886fe5ddd1a94859235626d9
pabloporzio <pabloporzio97@gmail.com>
2021-10-13 18:27:19 -0300

    Chapter 2 - Lesson 16 - Reacting to Param Changes

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="destination">
+    <section v-if="destination" class="destination">
         <h1>{{destination.name}}</h1>
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" alt="destination.name">
@@ -9,16 +9,28 @@
 </template>
 
 <script>
-import sourceData from '@/data.json';
-
 export default {
+    data() {
+        return {
+            destination: { },
+        }
+    },
     computed: {
         destinationId() {
             return parseInt(this.$route.params.id);
         },
-        destination() {
-            return sourceData.destinations.find(destination => destination.id === this.destinationId);
-        }
+    },
+    methods: {
+        async initData() {
+            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}`);
+            this.destination = await response.json();
+        },
+    },
+    async created() {
+        this.initData();
+        // Chapter 2 - Lesson 16 - React to Param Changes:
+        // another approach to re-fire the hook when the view changes, instead of adding a key to the router-view in App.vue
+        // this.$watch(() => this.$route.params, this.initData); 
     }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
         <h1>All Destinations</h1>
-        <div class="destinations">
+        <div v-if="destinations.length > 0" class="destinations">
             <router-link
                 v-for="destination in destinations"
                 :key="destination.id"
@@ -15,13 +15,29 @@
 </template>
 
 <script>
-    import sourceData from '@/data.json';
-
     export default {
         data () {
             return {
-                destinations: sourceData.destinations,
+                destinations: [],
             }
+        },
+        /**
+         * Chapter 2 - Lesson 16:
+         * 
+         * This hook isn't part of the lesson, but I wanted to use the dummy API for all the data because
+         * it's more real than using a JSON. Since there is no endpoint to get all the data at once, I had
+         * to manually fetch the data for each country. It's not ideal, but it's the only workaround.
+         */
+        async created() {
+            const destinations = [];
+            const countries = ['brazil', 'hawaii', 'panama', 'jamaica'];
+
+            for (let country of countries) {
+                const countryData = await fetch(`https://travel-dummy-api.netlify.app/${country}`);
+                destinations.push(await countryData.json())
+            }
+            
+            this.destinations = destinations;
         }
     };
 </script>
